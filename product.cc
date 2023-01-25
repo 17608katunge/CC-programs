8e34b02a0199a95e3510575e03c516e93f032f23
17608katunge <123051164+17608katunge@users.noreply.github.com>
2023-01-25 14:30:07 +0300

    Add files via upload

---
+++ product.cc
@@ -0,0 +1,13 @@
+//product of two numbers
+#include<stdio.h>
+int main()
+{
+int a,b,product;
+printf("\n enter value of a:");
+scanf("%d,&a");
+printf("\n enter value of b:");
+scanf("%d,&b");
+product=a*b;
+printf("the product=%d",product);
+return 0;
+}
